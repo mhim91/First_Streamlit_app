1ba66fee9260b78bd40070ccf56f99ddfce4f446
mhim91 <42978066+mhim91@users.noreply.github.com>
2026-02-20 10:12:40 +0000

    Save changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,123 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+##simulation functions
+# 
+# 
+
+# Initialize the variable in session_state if it doesn't exist yet
+if 'time' not in st.session_state:
+    st.session_state.time = 0
+if 'x' not in st.session_state:
+    st.session_state.x = 1
+if 'y' not in st.session_state:
+    st.session_state.y = 0
+if 'vx' not in st.session_state:
+    st.session_state.vx = 0
+if 'vy' not in st.session_state:
+    st.session_state.vy = 1
+if 'x_list' not in st.session_state:
+    st.session_state.x_list = [1]
+if 'y_list' not in st.session_state:
+    st.session_state.y_list = [0]
+if 'G' not in st.session_state:
+    st.session_state.G = 1
+if 'M' not in st.session_state:
+    st.session_state.M = 1
+if 'dt' not in st.session_state:
+    st.session_state.dt = 0.0001
+
+#functions
+
+def Period(a):
+    return np.sqrt(4 * np.pi**2 * a**3 / (st.session_state.G*st.session_state.M))
+
+def Acc(x,y, x0 = 0, y0 = 0, M = st.session_state.M):
+    d = np.sqrt((x - x0)**2 + (y - y0)**2)
+    k = st.session_state.G*M/(d**3)
+    return np.array([-(x - x0)*k, -(y - y0)*k])
+
+
+def step(x,y,vx, vy, dt):
+    #xm, ym = moon_coordinates(t)
+
+    acc = Acc(x,y) #+ Acc(x,y, xm, ym, moon_M)
+    ax = acc[0]
+    ay = acc[1]
+
+    vx_0 = vx
+    vy_0 = vy
+
+    vx = vx + ax*dt
+    vy = vy + ay*dt
+
+    x = x + (vx +  vx_0)*dt / 2
+    y = y + (vy +  vy_0)*dt / 2
+
+    return x,y,vx,vy
+
+def vis_viva_vel(r,a):
+    return np.sqrt(st.session_state.G*st.session_state.M*(2/r - 1/a))
+
+def tangential_boost(vx,vy, Dv):
+    v = np.sqrt(vx**2 + vy**2)
+    return (vx/v)*(v + Dv), (vy/v)*(v + Dv)
+
+def velocity(vx,vy):
+    return np.sqrt(vx**2 + vy**2)
+
+
+
+def update_simulation():
+    newtime = st.session_state.time + wait_time_input
+    while st.session_state.time < newtime:
+        st.session_state.x, st.session_state.y, st.session_state.vx, st.session_state.vy = step(st.session_state.x, st.session_state.y, st.session_state.vx, st.session_state.vy, st.session_state.dt)
+        st.session_state.x_list.append(st.session_state.x)
+        st.session_state.y_list.append(st.session_state.y)
+        st.session_state.time += st.session_state.dt
+    
+
+def return_figure():
+    fig, ax = plt.subplots()
+    ax.plot(st.session_state.x_list[::1000], st.session_state.y_list[::1000], label = "bana")
+    ax.set_aspect('equal', adjustable='box')
+    ax.set_box_aspect(1)
+    plt.grid(True)
+    ax.set_xlim([-10, 10])
+    ax.set_ylim([-10, 10])
+    return fig
+
+
+
+#gui code
+
+
+st.title("Hohmannbanor")
+
+# Example matplotlib plot (renders in Streamlit)
+st.pyplot(return_figure())
+
+
+st.write("Aktuellt tillstånd: x = ", st.session_state.x,
+         "y = ", st.session_state.y,
+         "vx = ", st.session_state.vx,
+         "vy = ", st.session_state.vy,
+         "v = ", velocity(st.session_state.vx, st.session_state.vy))
+
+
+boost_input = st.number_input('Boost ammount:', placeholder = 0)
+wait_time_input = st.number_input('Wait time', placeholder = 1)
+
+st.button("Reset", type="primary")
+
+if st.button("Implementera"):
+    update_simulation()
+    st.rerun()
+
+    st.write(st.session_state.time)
+
+
+
+
+
